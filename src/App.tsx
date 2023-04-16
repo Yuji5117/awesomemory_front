bbf46b03c8959c599d025afe497d4f81a6b7000a
Yuji Nakamura <ny5117@icloud.com>
2023-04-16 11:02:49 +0900

    axiosを導入し、API通信しアルバム情報を取得できるように修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import { Album } from "./types";
+import axios from "axios";
+
 function App() {
-  return <div className="App">Hello</div>;
+  const [albums, setAlbums] = useState<Album[]>([]);
+
+  useEffect(() => {
+    const getAlbums = async () => {
+      const res = await axios.get("http://localhost:3000/albums");
+
+      setAlbums(res.data);
+    };
+
+    getAlbums();
+  }, []);
+
+  return (
+    <div className="App">
+      {albums.map((album) => (
+        <div key={album.id}>
+          <p>{album.id}</p>
+          <p>{album.title}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
