f67bdc448225dc117dbf8117e05c6c703266c4d4
Yuji Nakamura <ny5117@icloud.com>
2023-05-04 21:57:00 +0900

    input fieldの共通コンポーネントを実装

--- src/components/Elements/Form/InputField.tsx
+++ src/components/Elements/Form/InputField.tsx
@@ -1,1 +1,15 @@
-export const InputField = () => <input />;
+import { UseFormRegisterReturn } from "react-hook-form";
+import styled from "styled-components";
+
+type InputFieldProps = {
+  type: "text" | "email" | "password";
+  regitration: UseFormRegisterReturn;
+};
+
+export const InputField = ({ type, regitration }: InputFieldProps) => {
+  return <StyledInput type={type} {...regitration} />;
+};
+
+const StyledInput = styled.input`
+  width: 200px;
+`;
