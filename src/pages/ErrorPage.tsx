f8c3dfec17be6d4a34692c8a8e2f3b3885011706
Yuji Nakamura <ny5117@icloud.com>
2023-05-04 09:06:59 +0900

    エラーページのerrorオブジェクトの型エラーを回避

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,14 +1,24 @@
-import { Link, useRouteError } from "react-router-dom";
+import { isRouteErrorResponse, Link, useRouteError } from "react-router-dom";
 
 export const ErrorPage = () => {
   const error = useRouteError();
   console.error(error);
 
-  return (
-    <>
-      <div>エラーページ</div>
-      <p>{error.statusText || error.message}</p>
-      <Link to="/">Homeページへ</Link>
-    </>
-  );
+  if (isRouteErrorResponse(error)) {
+    return (
+      <>
+        <div>エラーページ</div>
+        <p>{error.status}</p>
+        <p>{error.statusText}</p>
+        <Link to="/">Homeページへ</Link>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <p>何かしらのエラーが発生しています。</p>
+        <Link to="/">Homeページへ</Link>
+      </>
+    );
+  }
 };
